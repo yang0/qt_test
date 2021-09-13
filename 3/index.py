40ace891de6dd6d8441f6c461c4755918b07a1ca
unknown <yang0@126.com>
2021-09-13 19:48:39 +0800

    a

---
+++ 3/index.py
@@ -0,0 +1,30 @@
+import sys
+from PySide6.QtWidgets import QApplication, QBoxLayout, QDialog, QHBoxLayout, QLineEdit, QPushButton, QVBoxLayout
+
+class Form(QDialog):
+    
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        self.setWindowTitle("My Form")
+        self.edit = QLineEdit("Write my name here..")
+        self.button = QPushButton("Show Greetings")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+        self.button.clicked.connect(self.greetings)
+
+    # Greets the user
+    def greetings(self):
+        print(f"Hello {self.edit.text()}")
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec())        
